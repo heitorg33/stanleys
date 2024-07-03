4d2efad9519dd5b53faa67a6c0a167cba578a451
Heitor Santos <heitorg33@gmail.com>
2024-07-03 11:44:48 -0300

    Atualização da variável de telefone

--- src/controller/controllerNumber.js
+++ src/controller/controllerNumber.js
@@ -2,7 +2,7 @@ const { validarNumber } = require("../service/servicehs");
 
 const controllerNumber = async (req, res) => {
   try {
-    const requestNumber = await validarNumber(req.body.number_wts);
+    const requestNumber = await validarNumber(req.body.phone);
     return res.status(200).json(requestNumber);
   } catch (error) {
     return res.status(error.response.status).json(error.message);
