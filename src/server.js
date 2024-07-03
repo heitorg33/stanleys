87c26c5dd8888c2e3fc3ab954764b753c1e492aa
Heitor Santos <heitorg33@gmail.com>
2024-07-03 16:05:17 -0300

    Correção cors

--- src/server.js
+++ src/server.js
@@ -1,12 +1,21 @@
-const express = require('express')
-const cors = require('cors');
-const { router } = require('./router/routes');
-const app = express()
-const port = 3000
+const express = require("express");
+const cors = require("cors");
+const { router } = require("./router/routes");
+const app = express();
+const port = 3000;
 
 app.use(express.json());
 app.use(cors());
-app.use(router)
+app.use(router);
 
-app.get('/', (req, res) => res.send('Hello World!'))
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*"); // Permite todas as origens
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept"
+  );
+  next();
+});
+app.get("/", (req, res) => res.send("Hello World!"));
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
